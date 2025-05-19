0fe5fd04cce7833f143bcd653b39d32e5b8fba98
Petras Vestartas <petrasvestartas@gmail.com>
2025-05-19 14:47:59 +0200

    Update class_primitives.py

--- {{cookiecutter.project_folder}}/tutorial/class_primitives.py
+++ {{cookiecutter.project_folder}}/tutorial/class_primitives.py
@@ -1,17 +1,7 @@
 from {{cookiecutter.project_slug}}.class_primitives import Data
 
-# Different ways to create Data objects
-sphere = Data("sphere", 42)           # Regular constructor
-cube = Data.from_name("cube")        # Using just a name
-cylinder = Data.from_values("cylinder", 99)  # Explicit values
-
-# Print out all objects
+sphere = Data("sphere", 42)           
 print(f"Sphere: {sphere.name}, {sphere.value}")
-print(f"Cube: {cube.name}, {cube.value}")
-print(f"Cylinder: {cylinder.name}, {cylinder.value}")
-
-# Modify some properties
-cylinder.name = "cone"
-cylinder.value = 77
-print(f"\nAfter modification: {cylinder.to_string()}")
-
+sphere.name = "cone"
+sphere.value = 77
+print(f"\nAfter modification: {sphere.to_string()}")
