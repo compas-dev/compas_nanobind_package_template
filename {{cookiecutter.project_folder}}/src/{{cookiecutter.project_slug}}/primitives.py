9076a7de239c7c7299392c814562b66408d83169
Petras Vestartas <petrasvestartas@gmail.com>
2025-05-20 14:26:15 +0200

    Update primitives.py

--- {{cookiecutter.project_folder}}/src/{{cookiecutter.project_slug}}/primitives.py
+++ {{cookiecutter.project_folder}}/src/{{cookiecutter.project_slug}}/primitives.py
@@ -2,5 +2,23 @@ from {{cookiecutter.project_slug}} import _primitives  # The actual C++ module
 
 
 def add(a, b):
-    """Add two numbers together."""
+    """Add two numbers together.
+
+    Parameters
+    ----------
+    a : float
+        The first number.
+    b : float
+        The second number.
+
+    Returns
+    -------
+    float
+        The sum of the two numbers.
+
+    Examples
+    --------
+    >>> add(2.0, 3.0)
+    5.0
+    """
     return _primitives.add(a, b)
